f8710f6f2fd5659eaddc516d935c27a085e15d15
taitus <sebastia.roig@gmail.com>
2016-10-10 13:39:47 +0200

    Add role to User. Generate devise views [#5]

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  extend Enumerize
+
+  enumerize :role, in: [:owner, :admin], scope: true, predicates: true       
+    
 end

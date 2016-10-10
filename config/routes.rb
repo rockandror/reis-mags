94972fce9a8b7fbefb3cc4379ee520babe81d4cf
taitus <sebastia.roig@gmail.com>
2016-10-10 16:49:44 +0200

    Add routes [#10]

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   
   get '/pages/cookies_policy' => 'pages#cookies_policy', :as => :cookies_policy_page
+
+  root "pages#show", page: "home"
 end

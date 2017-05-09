e00d1aa3958f9bfa73a3446175ff3f9577844179
Alessandro Cuoghi <agileontheweb@gmail.com>
2017-05-09 19:43:54 +0200

    Add conditional if fontawesome or custom svg change icon in Datapicker and timepicker [#13]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   def set_locale
    I18n.locale = params[:locale] || I18n.default_locale
    @block_landscape = false # If true: rotate smartphone on Landscape show a message.
+   @font_awesome = true # If true: If true use a FontAwesome else use custom svg icon
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     "flags/#{locale}.png"
   end
 
+  def custom_icon(icon, klass = "text-default")
+    "<svg class=\"icon-svg #{klass} #{icon}\"><use xlink:href=\"##{icon}\"/></svg>".html_safe
+  end
+
 end

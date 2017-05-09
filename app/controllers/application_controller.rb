9be076e9578d9226c6bdd2ff9b4547ea3fbbd7b8
Alessandro Cuoghi <agileontheweb@gmail.com>
2017-05-09 17:20:33 +0200

    Add variables @block_landscape, if true show message landscape. [#13]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   before_filter :set_locale
- 
+
   protect_from_forgery with: :exception
- 
+
   private
 
   def set_locale
    I18n.locale = params[:locale] || I18n.default_locale
+   @block_landscape = false # If true: rotate smartphone on Landscape show a message.
   end
-  
+
 end

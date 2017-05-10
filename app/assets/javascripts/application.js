e3d13175da9a962e66477ea5e2515930c372a509
Alessandro Cuoghi <agileontheweb@gmail.com>
2017-05-10 12:33:40 +0200

    Some refactor mixin calendarpicker and timepicker. Some style select bootstrap. [#13]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,8 +42,8 @@ $(function() {
     disableFocus: true
   });
 
-  $('.selectpicker').selectpicker ();
-  $('.minicolors').minicolors()
+  $('select').selectpicker();
+  $('.minicolors').minicolors();
 
 });
 

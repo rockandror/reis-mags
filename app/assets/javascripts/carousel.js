2eca41d528ddce528d13c7beab2449220088e972
Alessandro Cuoghi <agileontheweb@gmail.com>
2017-05-12 19:21:22 +0200

    Carousel grid. [#13]

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,5 +1,26 @@
 function carousel(){
+
+  //carousel_thumbnail
   $('#carousel_thumbnail').carousel({
-    interval: 10000
+    interval: 7000
   })
+
+  //carousel_grid_slide
+  $('#carousel_grid_slide').carousel({
+    //interval: 5000
+  });
+  $('[id^=carousel-selector-]').click(function () {
+    var id_selector = $(this).attr("id");
+    try {
+      var id = /-(\d+)$/.exec(id_selector)[1];
+      console.log(id_selector, id);
+      jQuery('#carousel_grid_slide').carousel(parseInt(id));
+    } catch (e) {
+      console.log('Regex failed!', e);
+    }
+  });
+  $('#carousel_grid_slide').on('slid.bs.carousel', function (e) {
+    var id = $('.item.active').data('slide-number');
+    $('#carousel-text').html($('#slide-content-'+id).html());
+  });
 }

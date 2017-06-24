a813d2e2160bea4a4cbeaf836b92800ca8359df7
Alessandro Cuoghi <agileontheweb@gmail.com>
2017-06-24 17:28:14 +0200

    Passed init-scss test into the folder mixin/partials. [#30]

--- app/assets/javascripts/nprogress-config.js
+++ app/assets/javascripts/nprogress-config.js
@@ -3,3 +3,15 @@ NProgress.configure({
   ease: 'ease',
   speed: 500
 });
+
+function nprogress(){
+  console.log("porogressssss");
+  if($('#nprogress').lenght > 0) {
+    console.log("NPROGRESSSS");
+    $('#nprogress').addClass('nprogress');
+  }
+}
+
+$(document).on('page:fetch',   function() {
+   nprogress();
+ });

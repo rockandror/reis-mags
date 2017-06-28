b5878b8bcfd00956edc3a69b9a3c803249930938
Alessandro Cuoghi <agileontheweb@gmail.com>
2017-06-28 11:12:05 +0200

    Changed id for a class. Move partial scroll_top  from footer to application. Show the buttons after scroll page. [#27]

--- app/assets/javascripts/scroll_top.js
+++ app/assets/javascripts/scroll_top.js
@@ -1,6 +1,15 @@
 function scroll_top(){
   console.log("Scroll top");
-  $('#scroll-top').each(function(){
+
+  $(window).scroll(function(){
+      if ($(this).scrollTop() > 100) {
+          $('.scroll-top').fadeIn();
+      } else {
+          $('.scroll-top').fadeOut();
+      }
+  });
+
+  $('.scroll-top').each(function(){
       $(this).click(function(){
           $('html,body').animate({ scrollTop: 0 }, 'slow');
           return false;

54ee61c38ef07c0dcadcf7a12622dea227be3d43
Senén Rodero Rodríguez <senenrodero@gmail.com>
2017-10-17 16:37:52 +0200

    Add CA translations and remove uneeded ones.

--- config/application.rb
+++ config/application.rb
@@ -19,9 +19,9 @@ module ReisMags
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.i18n.default_locale = :es
-    config.i18n.available_locales = [:es, :en]
-    config.i18n.locale = :es
+    config.i18n.default_locale = :ca
+    config.i18n.available_locales = [:ca]
+    config.i18n.locale = :ca
     config.i18n.enforce_available_locales = false
 
     # Do not swallow errors in after_commit/after_rollback callbacks.

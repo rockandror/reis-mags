b6e13a3e39886e2b9a626c356e98c80bb784fc9c
taitus <sebastia.roig@gmail.com>
2017-10-17 16:21:35 +0200

    Remove code

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Rails.application.routes.draw do
 
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     get "/pages/cookies_policy" => "pages#cookies_policy", as: :cookies_policy
-    unless Rails.env.production?
-      get "/components", to: "pages#show", page: "components", as: :components
-    end
     root "pages#show", page: "home"
   end
 end

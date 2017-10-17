c219b94cc7800e9a30e5f0c585ff7b09a3fbbb53
Senén Rodero Rodríguez <senenrodero@gmail.com>
2017-10-17 16:08:52 +0200

    Change application name

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Suspender
+module ReisMags
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -22,7 +22,7 @@ module Suspender
     config.i18n.default_locale = :es
     config.i18n.available_locales = [:es, :en]
     config.i18n.locale = :es
-    config.i18n.enforce_available_locales = false    
+    config.i18n.enforce_available_locales = false
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true

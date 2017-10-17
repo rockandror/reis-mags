e9b7f6975dd79e9c3957aa390e4cd6c8d65f3d17
taitus <sebastia.roig@gmail.com>
2017-10-17 16:51:02 +0200

    Add street model

---
+++ spec/model/street_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Street, :type => :model do
+
+  let!(:street) { build(:street) }
+
+  subject { street }
+
+  it { should be_valid }
+
+  context "validations" do
+
+    it "should not be valid without name" do
+      street.name = nil
+      expect(street).not_to be_valid
+    end
+
+    it "should not be valid without name" do
+      street.position = nil
+      expect(street).not_to be_valid
+    end
+
+  end
+
+end

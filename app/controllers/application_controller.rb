4b9cc71e78676635cc613cf3e371daeb2a8534a1
taitus <sebastia.roig@gmail.com>
2017-10-18 16:26:46 +0200

    After login redirect to admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,9 @@ class ApplicationController < ActionController::Base
   def set_icon_font
     @font_awesome = true
   end
+
+  protected
+    def after_sign_in_path_for(resource)
+      rails_admin_path
+    end
 end

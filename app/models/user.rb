a0eeedbf5354bc2a87108d42e924253ff39dc21b
taitus <sebastia.roig@gmail.com>
2017-10-18 16:13:05 +0200

    Remove registerable from devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   include UserAdmin
 
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   extend Enumerize
   enumerize :role, in: [:user, :admin], scope: true, predicates: true
